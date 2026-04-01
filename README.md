8495b1d0af108bfa43752a9526ccf8d19c6364cc
MASAKI <shinju0514yellow@gmail.com>
2026-04-01 23:03:00 +0900

    lesson3 まで

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # Python_Practice
-# Python_Practice
--- lesson2.py
+++ lesson2.py
@@ -7,7 +7,7 @@ print('hello. \nHow are you?')
 print(r'C:\name\name')
 
 print("########")
-print("""\
+print("""
 line1
 line2
 line3\
@@ -15,4 +15,18 @@ line3\
 print("#########")
 
 print('Hi.' * 3 + 'Mike')
-print('Py' 'thon')
+print('Py' + 'thon')
+
+prefix = 'Py'
+print(prefix + 'thon')
+
+# インデックスで表示
+word = 'python'
+print(word[0])
+print(word[1])
+print(word[-1])
+print(word[0:2])
+print(word[2:5])
+print(word[2:]) # 2番目から最後まで
+word[0] = 'j' + word[1:]
+print(word)
